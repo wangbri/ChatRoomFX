fb66fe2f3447c8c9d50319d52c76a247b4a95be9
Brian Wang <wangbri@gmail.com>
2017-04-14 14:54:16 -0500

    Add ClientMain and ClientObserver

--- ClientMain.java
+++ ClientMain.java
@@ -1,5 +1,60 @@
 package assignment7;
 
-public class ClientMain {
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+//import java.awt.*;
+import java.awt.event.*;
 
+public class ClientMain {
+	private JTextArea incoming;
+	private JTextField outgoing;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	
+
+	public void run() throws Exception {
+		setUpNetworking();
+	}
+
+	private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource")
+		Socket sock = new Socket("127.0.0.1", 4242);
+		InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+		reader = new BufferedReader(streamReader);
+		writer = new PrintWriter(sock.getOutputStream());
+		System.out.println("networking established");
+		Thread readerThread = new Thread(new IncomingReader());
+		readerThread.start();
+	}
+
+	class SendButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			writer.println(outgoing.getText());
+			writer.flush();
+			outgoing.setText("");
+			outgoing.requestFocus();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			new ClientMain().run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	class IncomingReader implements Runnable {
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					incoming.append(message + "\n");
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
